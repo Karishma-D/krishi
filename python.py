d7c14800700b49af26a6ee1e5ecfba13ef61e7aa
Karishma-D <57486076+Karishma-D@users.noreply.github.com>
2019-11-07 15:47:31 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,19 @@
 print("welcome to python")
 list=[1,2,3,4,5,6]
 list2=[]
+list3=[]
 for i in list: 
   p=i*2
   list2.append(p)
   
 print(list2)  
+
+
+for j in list2:
+  l=j/2
+  list3.append(l)
+  
+print(list3) 
+
+if list2==list3:
+  print("Kya chutiyapa hai")
